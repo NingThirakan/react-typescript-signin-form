c89588c537e22e1eded61933e1676b2fd077ed8c
thirakan_k <thirakan_k@banpu.co.th>
2024-12-23 09:39:21 +0700

    update theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { Box } from '@mui/material';
 import { ColorModeToggle } from './components/colorModeToggle';
 import { SigninForm } from './components/signinForm';
 
 function App() {
   return (
     <>
-      <ColorModeToggle />
+      <Box display='flex' justifyContent='flex-end' pt={2} pr={2}>
+        <ColorModeToggle />
+      </Box>
       <SigninForm />
     </>
   );
--- src/components/colorModeToggle.tsx
+++ src/components/colorModeToggle.tsx
@@ -1,76 +1,63 @@
 import { DarkMode, LightMode } from "@mui/icons-material"
-import { IconButton, Menu, MenuItem, useColorScheme } from "@mui/material"
-import { useCallback, useState } from "react"
+import { FormControlLabel, Radio, radioClasses, RadioGroup, useColorScheme } from "@mui/material"
 
 export const ColorModeToggle = () => {
   const { mode, setMode } = useColorScheme()
-  const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null)
-  const open = Boolean(anchorEl)
-
-  // const icon = {
-  //   light: <LightMode />,
-  //   dark: <DarkMode />,
-  // }[resolvedMode]
-
-  const handleClick = useCallback((event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget)
-  }, [])
-
-  const handleClose = useCallback(() => {
-    setAnchorEl(null);
-  }, [])
-
-  const handleMode = useCallback((mode: 'light' | 'dark') => () => {
-    setMode(mode)
-    handleClose()
-  }, [handleClose, setMode])
 
   if (!mode) {
     return null
   }
 
   return (
-    <>
-      <IconButton
-        onClick={handleClick}
-        disableRipple
-        size="small"
-        sx={{
-          borderRadius: "4px",
-          "&:hover, &:focus": {
-            backgroundColor: "action.hover",
+    <RadioGroup
+      defaultValue="dark"
+      row
+      aria-label="Color scheme"
+      name="color-scheme-segmented-control"
+      sx={{
+        display: "flex",
+        gap: '4px',
+        "& svg": { transform: "scale(0.8)", transition: "0.2s" },
+        [`& .${radioClasses.checked} svg`]: { transform: "scale(1)" },
+        [`& .${radioClasses.root}`]: {
+          width: '44px',
+          height: '44px',
+          border: "1px solid transparent",
+          borderRadius: "8px",
+          [`&.${radioClasses.checked}`]: {
+            border: "1px solid",
+            borderColor: "divider",
+            color: "text.primary",
           },
-        }}
-        aria-controls={open ? "color-scheme-menu" : undefined}
-        aria-haspopup="true"
-        aria-expanded={open ? "true" : undefined}
-      >
-        {/* {icon} */}
-      </IconButton>
-      <Menu
-        anchorEl={anchorEl}
-        id="account-menu"
-        open={open}
-        onClose={handleClose}
-        onClick={handleClose}
-        slotProps={{
-          paper: {
-            variant: "outlined",
-            sx: {
-              my: "4px",
-            },
-          },
-        }}
-        transformOrigin={{ horizontal: "right", vertical: "top" }}
-        anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
-      >
-        <MenuItem selected={mode === "light"} onClick={handleMode("light")}>
-          Light
-        </MenuItem>
-        <MenuItem selected={mode === "dark"} onClick={handleMode("dark")}>
-          Dark
-        </MenuItem>
-      </Menu>
-    </>
+        },
+        "& label": { margin: 0 },
+      }}
+      onChange={(event) => setMode(event.target.value as "light" | "dark")}
+    >
+      <FormControlLabel
+        value="light"
+        control={
+          <Radio
+            color="default"
+            disableTouchRipple
+            checkedIcon={<LightMode />}
+            icon={<LightMode />}
+          />
+        }
+        label=""
+      />
+      <FormControlLabel
+        value="dark"
+        control={
+          <Radio
+            color="default"
+            disableTouchRipple
+            checkedIcon={<DarkMode />}
+            icon={<DarkMode />}
+          />
+        }
+        label=""
+      />
+    </RadioGroup>
   )
 }
